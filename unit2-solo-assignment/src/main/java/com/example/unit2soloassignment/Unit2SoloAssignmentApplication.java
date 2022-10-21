3b741f32f7b7340e1eeaae588c0b5f35b6e163b4
KANG-MoonSoo <107465049+KANG-MoonSoo@users.noreply.github.com>
2022-10-21 19:35:10 +0900

    Add files via upload

---
+++ unit2-solo-assignment/src/main/java/com/example/unit2soloassignment/Unit2SoloAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.unit2soloassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Unit2SoloAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Unit2SoloAssignmentApplication.class, args);
+	}
+
+}
---
+++ unit2-solo-assignment/src/test/java/com/example/unit2soloassignment/Unit2SoloAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unit2soloassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Unit2SoloAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
